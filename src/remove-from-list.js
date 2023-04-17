c4ad75a74abecec037d3cd2f894806e7835d3a17
Olha Stanislavska <olga.stanislavskaya@gmail.com>
2023-04-17 23:49:14 +0300

    feat: add stack.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -15,16 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ */ 
+// class ListNode {
+//   constructor(x) {
+//     this.value = x;
+//     this.next = null;
+//   }
+// }
+
+function removeKFromList(l, k) {
+  let current = l.head;
+  let prev = null;
+  let index = 0;
+  while (index < l.length) {
+    if (current.value === k) {
+      if (index === 0) {
+        l.head = current.next;
+        current = current.next;
+      } else if (index === l.length - 1) {
+        current = null;
+        prev.next = null;
+      } else {
+        prev.next = current.next;
+      }
+      l.length--; 
+      index++;
+    } else {
+      prev = current;
+      current = current.next;
+      index++;
+    }
+  }
+  return l;
 }
 
 module.exports = {
--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.s = [];
+  }
+  push(element) {
+    this.s.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.s.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.s.length ? this.s[this.s.length - 1] : undefined;
   }
 }
 
